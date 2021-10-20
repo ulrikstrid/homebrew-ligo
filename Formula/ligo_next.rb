eaeb51096fff8db91d6c6d9634e2e82f92533b39
Ulrik Strid <ulrik.strid@outlook.com>
2021-10-20 15:11:27 +0200

    add ligo_next

---
+++ Formula/ligo_next.rb
@@ -0,0 +1,52 @@
+class LigoNext < Formula
+  desc "Friendly Smart Contract Language for Tezos"
+  homepage "https://ligolang.org/"
+
+  version "6ea975875373f2094136182f0e7da26d95e8f96f"
+  url "https://gitlab.com/ligolang/ligo/-/archive/6ea975875373f2094136182f0e7da26d95e8f96f/ligo-6ea975875373f2094136182f0e7da26d95e8f96f.tar.gz"
+  # Version is autoscanned from url, we don't specify it explicitly because `brew audit` will complain
+  # To calculate sha256: 'curl -L --fail <url> | sha256sum'
+  sha256 "28602bbb13ce57861abad158c11534025479a96640c6f71af13bd4328861c201"
+
+  build_dependencies = %w[opam rust hidapi pkg-config]
+  build_dependencies.each do |dependency|
+    depends_on dependency => :build
+  end
+
+  dependencies = %w[gmp libev libffi]
+  dependencies.each do |dependency|
+    depends_on dependency
+  end
+
+  # sets up env vars for opam before running a command
+  private def with_opam_env(cmd)
+    "eval \"$(opam config env)\" && #{cmd}"
+  end
+
+  def install
+    # ligo version is taken from the environment variable in build-time
+    ENV["LIGO_VERSION"] = LigoNext.version
+    # avoid opam prompts
+    ENV["OPAMYES"] = "true"
+
+    # init opam state in ~/.opam
+    system "opam", "init", "--bare", "--auto-setup", "--disable-sandboxing"
+    # create opam switch with required ocaml version
+    system "opam", "switch", "create", ".", "ocaml-base-compiler.4.10.2", "--no-install"
+    # build and test external dependencies
+    system with_opam_env "opam install --deps-only --with-test --locked ./ligo.opam $(find vendors -name \\*.opam)"
+    # build vendored dependencies
+    system with_opam_env "opam install $(find vendors -name \\*.opam)"
+    # build ligo
+    system with_opam_env "dune build -p ligo"
+
+    # install ligo binary
+    cp "_build/install/default/bin/ligo", "ligo"
+    bin.mkpath
+    bin.install "ligo"
+  end
+
+  test do
+    system "#{bin}/ligo", "--help=plain"
+  end
+end
